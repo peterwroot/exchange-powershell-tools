a03918f2ad64289f637511e5025beafa470cf002
Peter Wroot <peter.wroot@infinitymail.co.uk>
2019-01-14 11:24:51 +0000

    Added 'Get-MailboxSizes.ps1'

---
+++ Get-MailboxSizes.ps1
@@ -0,0 +1,18 @@
+﻿$File = "C:\MailboxSizes.csv"
+
+if(test-path $File){
+    Remove-Item $File
+}
+
+Add-Content -Path $File -Value "Mailbox,Size"
+
+get-mailbox | foreach{
+
+$MailboxStatUser = $_.PrimarySMTPAddress
+$MailboxStatSize = Get-MailboxStatistics $_.Alias | Select-Object -ExpandProperty TotalItemSize 
+
+$CSVOutLine = "$MailboxStatUser,$MailboxStatSize"
+ 
+Add-Content -Path $File -Value $CSVOutLine
+
+}
